157510d592e9821be3d534bff3fbd42f47add6a8
Voronov <d.a.voronov550@gmail.com>
2025-03-11 20:07:47 +0300

    Migrations for authentication fixed

--- backend/apps/authentication/__init__.py
+++ backend/apps/authentication/__init__.py
@@ -1,2 +1,1 @@
 default_app_config = 'apps.authentication.apps.AuthenticationConfig'
-from apps.authentication.infrastructure.models import *
--- backend/apps/authentication/apps.py
+++ backend/apps/authentication/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.authentication'
+
+    def ready(self):
+        # Импортируем модели только внутри ready()
+        from .infrastructure import models
